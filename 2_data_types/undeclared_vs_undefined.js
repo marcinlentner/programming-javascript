f978bd7b4e34b57db5584b107617a10c31f6f5d8
Marcin Lentner <marcin_len@yahoo.co.uk>
2021-02-21 17:26:57 +0000

    Undeclared VS Undefined

--- 2_data_types/undeclared_vs_undefined.js
+++ 2_data_types/undeclared_vs_undefined.js
@@ -21,7 +21,7 @@ if (boolean_true) {
  * typeof always returns a string
  */
 
-//console.log(undeclared_value);
+console.log(undeclared_value);
 // ERROR! Value is not defined.
 
 console.log(typeof undeclared_value);
@@ -31,6 +31,7 @@ console.log(typeof undeclared_value);
  * Check for undeclared value
  */
 if (undeclared_value) {
+  // this block will not execute
   // REFERENCE ERROR! VALUE NOT DEFINED!
 }
 
@@ -41,30 +42,40 @@ if (typeof undeclared_value) {
 }
 
 /*
- * Undefined values - pointer to value exists, no value assigned
+ * Undefined values - pointer to value in memory exists, no value assigned
  */
 let undefined_value;
 console.log(undefined_value);
 // undefined
 
-//let undefined_value; // has value of undefined
-// aka
-let undefined_value_defined = undefined;
-
 if (undefined_value) {
   // this block will not execute
 }
 
+/*
+ * Assign undefined value
+ */
+let undefined_value_defined = undefined;
+
+/*
+ * Compare two undefined values
+ */
 if (undefined_value == undefined_value_defined) {
   // this block will execute
 }
 
+/*
+ * Undefined string aka Empty String
+ */
 let string_val = "";
 
 if (string_val) {
   // this block will not execute
 }
 
+/*
+ * Undefined object aka Null object
+ */
 // define object to be used later
 let object_val = null;
 
